b7e3089ededd6414fa8f26ebaad8a8b415f5c56c
quentin.aubry <aubryquentin700@gmail.com>
2019-02-11 23:28:37 +0100

    SUPPRESSION de salut c est moi dans les membres

--- frontend/app/src/pages/Card1.js
+++ frontend/app/src/pages/Card1.js
@@ -13,7 +13,7 @@ class Card1 extends Component {
         </div>
         <div className="col-xs-12 col-sm-3"></div>
         <div class="col-xs-12 col-sm-6">
-          <h5 className="font-weight-bold mt-4 mb-3">Salut moi c'est, {this.props.nom} {this.props.prenom} </h5>
+          <h5 className="font-weight-bold mt-4 mb-3">{this.props.nom} {this.props.prenom} </h5>
           <p className="text-uppercase blue-text"><strong>Membre</strong></p>
           <p className="grey-text">{this.props.description}</p>
           <p className="grey-text"><a className="fa fa-comments text-muted c-info" href={"mailto:"+this.props.mail}>Contact moi</a></p><br/><br/>
