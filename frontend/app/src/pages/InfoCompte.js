86792d542199c6fb328c1e6cd602155450b6f989
unknown <massi.ouldrabah@crie.i-univ-tlse2.fr>
2019-02-12 15:55:39 +0100

    merging

--- frontend/app/src/pages/InfoCompte.js
+++ frontend/app/src/pages/InfoCompte.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import Mdp from './Mdp.js';
 import NavbarMembres from './NavbarMembres.js';
+import Modal from 'react-responsive-modal';
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+
 
 class InfoCompte extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          adresse: ''
+          adresse: '',
+          open: false
+          
         };
+        
     
         this.handleSubmit = this.handleSubmit.bind(this);
       }
@@ -16,15 +25,50 @@ class InfoCompte extends Component {
         alert('Adresse: '+this.state.adresse);
         event.preventDefault();
     }
+    
+  
+    onOpenModal = () => {
+      this.setState({ open: true });
+    };
+
+    modifierprofil(){
+      
+    }
+  
+    onCloseModal = () => {
+      this.setState({ open: false });
+    };
 
     render() {
-        return (
-                <div>
-                    <NavbarMembres/>
-                    <Mdp/>
-                </div>
-        );
-      }
+      const { open } = this.state;
+      return (
+        <div style={styles}>
+        <NavbarMembres/>
+          
+          <button onClick={this.onOpenModal}>Mes informations </button>
+          <Modal open={open} onClose={this.onCloseModal} center>
+            <h2>Mon Profil</h2>
+            <p>
+              Nom : <br/>
+              Prenom:<br/>
+              email:<br/>
+              Telephone:<br/>
+              Description:<br/>
+              Identifiant:<br/>
+
+            </p>
+            
+            <button onClick={this.handleClose} color="primary">
+              Modifier
+            </button>
+            <button onClick={this.handleClose} color="primary">
+              Fermer
+            </button>
+          
+          </Modal>
+        </div>
+      );
+    }
 }
 
 export default InfoCompte;
