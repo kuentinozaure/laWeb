9b45467e880137c26bf6574c4ff988a1c2170620
unknown <you@example.com>
2019-02-14 12:18:44 +0100

    plus de commentaire

--- frontend/app/src/pages/Accueil.js
+++ frontend/app/src/pages/Accueil.js
@@ -2,49 +2,7 @@ import React, { Component } from 'react';
 import "./Accueil.css";
 class Accueil extends Component {
   render() {
-    return (
-    //     <div className="container">
-    //     <div className="row">
-    //         <div className="col-xs-12 col-sm-12 col-md-4 col-lg-8 col-md-offset-2">
-    //                        <p>
-    //                            <div id="div_titreAccueil">
-    //                                  ~ Qu'est-ce que LaWeb ? ~
-    //                              </div>
-    //                              <br/>
-    //                              <div id="div_presentation" align="center">
-                                    //  LaWeb est une association constituée d'étudiants et d'enseignants
-                                    //  de l'université Jean Jaurès située à Toulouse.<div id="sautdeligne"></div>
-                                    //  Elle a pour but d'aider et d'apporter de nouvelles connaissances
-                                    //  dans des domaines autour de l'informatique.<div id="sautdeligne"></div>
-                                    //  Elle propose un certain nombre d'activités ayant pour but d'initier
-                                    //  ou d'approfondir vos connaissances autour d'ateliers et d'évènements.<div id="sautdeligne"></div>
-    //                         </div>
-    //                        </p>
-    //                        <br></br>
-    //         </div>
-    //     </div>
-    //      <br>
-    //      </br>
-    //  </div>
-    //<article>
-    //         Fondé par des :
-    //           <ul>
-    //             <li>
-    //               Étudiants
-    //             </li>
-    //             <li>
-    //               Ensaignant
-    //             </li>
-    //           </ul> 
-    //         En :
-    //           <ul>
-    //             <li>
-    //               2018
-    //             </li>
-    //           </ul>
-    //       </article> 
-    
-
+    return ( 
     <div className="container">
       <div className="row  div-zone1">
         <img src="../../images/laWeb-logo-accueil.png" alt="Smiley face" className="laWeb-logo-accueil col-xs-4 col-sm-12 col-lg-6"/> 
