b69d010dff42b17922728e181b9ed8ae2fd62de3
Emma Dos Santos <emmados@orange.fr>
2019-02-25 15:11:42 +0100

    FIX delete astuce

--- frontend/app/src/pages/ActiviteNonValide.js
+++ frontend/app/src/pages/ActiviteNonValide.js
@@ -4,11 +4,14 @@ import { SERVER_URL } from "../consts";
 
 import axios from 'axios';
 
+import { connect } from 'react-redux';
+
 class ActiviteNonValide  extends React.Component {
     constructor(props) {
         super(props);
 
         this.handleDelete = this.handleDelete.bind(this);
+        this.handleValidate = this.handleValidate.bind(this);
 
         this.state = {
             id:0,
@@ -17,14 +20,13 @@ class ActiviteNonValide  extends React.Component {
 
     handleDelete() {
 
-        axios.delete(SERVER_URL + "activity/"+this.props.id+"/")
-        // console.log(this.props.id)
-        // .then(response => {
-        //     console.log("Activité supprimé")
-        // })
-        // .catch(error => {
-        //     console.log(error);
-        // });
+        axios.delete(SERVER_URL + "activity/"+this.props.id+"/");
+    
+    }
+
+    handleValidate() {
+
+        axios.put(SERVER_URL + "activity/"+this.props.id+"/"+this.props.sessionConnect.id+"/");
     
     }
 
@@ -42,10 +44,10 @@ class ActiviteNonValide  extends React.Component {
                 <td>{this.props.salle}</td>
                 <td>{this.props.nombrePlaceDispo}</td>
                 <td className="text-right">
-                <a className='btn btn btn-info btn-sm' href="#">
+                <a className='btn btn btn-info btn-sm' href="#listeActiviteAdmin" onClick={this.handleValidate}>
                         Valider
                 </a>
-                <a className='btn btn btn-danger btn-sm' href="#listeActiviteAdmin" onClick={this.handleDelete()}>
+                <a className='btn btn btn-danger btn-sm' href="#listeActiviteAdmin" onClick={this.handleDelete}>
                         Refuser
                 </a>  
                 </td>
@@ -57,4 +59,8 @@ class ActiviteNonValide  extends React.Component {
       }
 }
 
-export default ActiviteNonValide;
+const mapStateToProps = state => {
+    return { sessionConnect: state.sessionReducer}
+  }
+
+  export default connect(mapStateToProps,null)(ActiviteNonValide)
