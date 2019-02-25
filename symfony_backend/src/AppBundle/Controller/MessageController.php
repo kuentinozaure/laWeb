45951d8911d7c8d4d5e9c1727323a92e91042b8c
unknown <massi.ouldrabah@crie.i-univ-tlse2.fr>
2019-02-26 00:02:43 +0100

    envoie de mails a la base de donnee

--- symfony_backend/src/AppBundle/Controller/MessageController.php
+++ symfony_backend/src/AppBundle/Controller/MessageController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\Message;
+use AppBundle\Entity\CategorieMessage;
 
 
 class MessageController extends Controller
@@ -22,7 +23,7 @@ class MessageController extends Controller
                       ->findAll();
       if (empty($message))
       {
-        return new JsonResponse(['message' => 'Invalid responsable member list not found'], Response::HTTP_NOT_FOUND);
+        return new JsonResponse(['message' => 'Message not found'], Response::HTTP_NOT_FOUND);
       }
               $formatted = [];
               foreach ($message as $message) {
@@ -41,6 +42,39 @@ class MessageController extends Controller
               }
       return new JsonResponse($formatted,Response::HTTP_OK);
   }
+     /**
+     * @Route("/categoriemessage/", name="categorie_message_list", methods={"GET"})
+     */
+    public function getUfr(Request $request)
+    {
+        $categoriemessage = $this->get('doctrine.orm.entity_manager')
+                        ->getRepository('AppBundle:CategorieMessage')
+                        ->findAll();
+
+        if (empty($categoriemessage))
+        {
+          return new JsonResponse(['message' => 'categoriemessage not found'], Response::HTTP_NOT_FOUND);
+        }
+                $formatted = [];
+                foreach ($categoriemessage as $categoriemessage) {
+                    $formatted[] = [
+                       'id' => $categoriemessage->getId(),
+                       'intitule' => $categoriemessage->getIntitule(),
+                    ];
+                }
+        return new JsonResponse($formatted,Response::HTTP_OK);
+    }
+
+
+
+
+
+
+
+
+
+
+
 
    /**
     * @Route("/message/{id}/", name="message_delete_once", methods={"DELETE"})
@@ -57,6 +91,43 @@ class MessageController extends Controller
             $em->flush();
             return new JsonResponse(['message' => 'Message deleted'],Response::HTTP_NOT_FOUND);
         }
+
+
+     /**
+     * @Route("/message1/", name="message_add", methods={"POST"})
+     */
+    public function addMessage(Request $request)
+    {
+        //get data from HTTP get method
+        $nom = $request->get('nom');
+        $prenom = $request->get('prenom');
+        $mail = $request->get('mail');
+        $message= $request->get('message');
+        $CategorieMessage = $request->get('CategorieMessage');
+        
+
+        //Check if one of all HTTP:GET value are empty
+        if(empty($nom) || empty($prenom) || empty($mail) || empty($message) || empty($CategorieMessage))
+         {
+           return new JsonResponse(['message' => 'NULL VALUES ARE NOT ALLOWED'], Response::HTTP_NOT_ACCEPTABLE);
+         }
+         $CategorieMessage = $this->get('doctrine.orm.entity_manager')
+                        ->getRepository('AppBundle:CategorieMessage')
+                        ->findById($CategorieMessage);
+
+
+           $messages = new Message();
+           $messages->setNom($nom)
+                    ->setPrenom($prenom)
+                      ->setMail($mail)
+                      ->setMessage($message)
+                      ->setidCategorieMessage($CategorieMessage[0]);
+
+           $em = $this->get('doctrine.orm.entity_manager');
+           $em->persist($messages);
+           $em->flush();
+           return new JsonResponse(['message' => 'message is added'], Response::HTTP_CREATED);
+    }
 
 }
 ?>
